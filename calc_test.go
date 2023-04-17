851cea727780a8cf3b52e6ef0b999e58176e0006
Andruixxd31 <andruix93@gmail.com>
2023-04-17 17:56:02 -0600

    added subtests

--- calc_test.go
+++ calc_test.go
@@ -12,13 +12,56 @@ type TestCase struct{
 }
 
 func TestCalculateIsArmstrong(t *testing.T){
-    testCase := TestCase{
-        value: 371,
-        expected: true,
-    }
+    t.Run("Should return true for 371", func(t *testing.T) {
+        testCase := TestCase{
+            value: 371,
+            expected: true,
+        }
 
-    testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
-    if testCase.actual != testCase.expected{
-        t.Fail()
-    }
+        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
+        if testCase.actual != testCase.expected{
+            t.Fail()
+        }
+    })
+
+
+    t.Run("Should return true for 370", func(t *testing.T) {
+        testCase := TestCase{
+            value: 370,
+            expected: true,
+        }
+
+        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
+        if testCase.actual != testCase.expected{
+            t.Fail()
+        }
+    })
+}
+
+
+func TestNegativeCalculateIsArmstrong(t *testing.T){
+    t.Run("Should fail for 350", func(t *testing.T) {
+        testCase := TestCase{
+            value: 350,
+            expected: false,
+        }
+
+        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
+        if testCase.actual != testCase.expected{
+            t.Fail()
+        }
+    })
+
+
+    t.Run("Should fail for 300", func(t *testing.T) {
+        testCase := TestCase{
+            value: 300,
+            expected: false,
+        }
+
+        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
+        if testCase.actual != testCase.expected{
+            t.Fail()
+        }
+    })
 }
