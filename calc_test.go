b5cc165f53ad528fb1231b6489a245dc5876064d
Andruixxd31 <andruix93@gmail.com>
2023-04-17 18:15:41 -0600

    added table driven testing example

--- calc_test.go
+++ calc_test.go
@@ -1,67 +1,60 @@
 package calc_test
 
 import (
-    "testing"
-    "github.com/andruixxd31/go-testing-bible"
+	"github.com/andruixxd31/go-testing-bible"
+	"testing"
 )
 
-type TestCase struct{
-    value int
-    expected bool
-    actual bool
+type TestCase struct {
+	name     string
+	value    int
+	expected bool
+	actual   bool
 }
 
-func TestCalculateIsArmstrong(t *testing.T){
-    t.Run("Should return true for 371", func(t *testing.T) {
-        testCase := TestCase{
-            value: 371,
-            expected: true,
-        }
+func TestCalculateIsArmstrong(t *testing.T) {
+	t.Run("Test for all 3 digits of armstrong", func(t *testing.T) {
+		tests := []TestCase{
+            TestCase{name: "Test for value: 370", value: 370, expected: true},
+			TestCase{name: "Test for value: 371", value: 371, expected: true},
+			TestCase{name: "Test for value: 407", value: 407, expected: true},
+			TestCase{name: "Test for value: 153", value: 153, expected: true},
+		}
 
-        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
-        if testCase.actual != testCase.expected{
-            t.Fail()
-        }
-    })
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				test.actual = calc.CalculateIsArmstrong(test.value)
+				if test.actual != test.expected {
+					t.Fail()
+				}
+			})
+		}
 
-
-    t.Run("Should return true for 370", func(t *testing.T) {
-        testCase := TestCase{
-            value: 370,
-            expected: true,
-        }
-
-        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
-        if testCase.actual != testCase.expected{
-            t.Fail()
-        }
-    })
+	})
 }
 
+func TestNegativeCalculateIsArmstrong(t *testing.T) {
+	t.Run("Should fail for 350", func(t *testing.T) {
+		testCase := TestCase{
+			value:    350,
+			expected: false,
+		}
 
-func TestNegativeCalculateIsArmstrong(t *testing.T){
-    t.Run("Should fail for 350", func(t *testing.T) {
-        testCase := TestCase{
-            value: 350,
-            expected: false,
-        }
+		testCase.actual = calc.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
 
-        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
-        if testCase.actual != testCase.expected{
-            t.Fail()
-        }
-    })
+	t.Run("Should fail for 300", func(t *testing.T) {
+		testCase := TestCase{
+			value:    300,
+			expected: false,
+		}
 
-
-    t.Run("Should fail for 300", func(t *testing.T) {
-        testCase := TestCase{
-            value: 300,
-            expected: false,
-        }
-
-        testCase.actual = calc.CalculateIsArmstrong(testCase.value) 
-        if testCase.actual != testCase.expected{
-            t.Fail()
-        }
-    })
+		testCase.actual = calc.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
 }
