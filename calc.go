e3fa3759d4f27a2e136a69f80547934df64c89d0
Andruixxd31 <andruix93@gmail.com>
2023-04-18 00:38:45 -0600

    tested coverage

--- calc.go
+++ calc.go
@@ -5,9 +5,17 @@ import (
 )
 
 func CalculateIsArmstrong(n int) bool {
-    a := n / 100
-    b := n %  100 / 10
-    c := n % 10
-    return n == int(math.Pow(float64(a), 3) + math.Pow(float64(b), 3) + math.Pow(float64(c), 3))
+	a := n / 100
+	b := n % 100 / 10
+	c := n % 10
+	return n == int(math.Pow(float64(a), 3)+math.Pow(float64(b), 3)+math.Pow(float64(c), 3))
 }
 
+// Random function
+func RandomFunction(n int) bool {
+	if n == 7 {
+		return true
+	} else {
+		return false
+	}
+}
